9029dfbc2e931cf6f797a8945f9bb4db96512d56
ffmcgee <jc.992@hotmail.com>
2022-05-22 21:56:18 +0000

    change favicon, meta title, manifest. Add loading placeholder when fetching tokens on select component.

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,9 +1,11 @@
+import { LoadingButton } from '@mui/lab';
 import {
   FormControl,
   InputLabel,
   MenuItem,
   Select as BaseSelect,
 } from '@mui/material';
+import { Container } from '@mui/system';
 import React from 'react';
 
 export const Select = ({ label, value, handleSelect, items }) => {
@@ -11,12 +13,19 @@ export const Select = ({ label, value, handleSelect, items }) => {
     <FormControl fullWidth>
       <InputLabel>{label}</InputLabel>
       <BaseSelect value={value} label={label} onChange={handleSelect}>
-        {items &&
+        {items ? (
           items.map(({ name, price }, i) => (
             <MenuItem key={i} value={name}>
               {name} - ${price.toFixed(2)}
             </MenuItem>
-          ))}
+          ))
+        ) : (
+          <Container
+            style={{ width: '100%', display: 'flex', justifyContent: 'center' }}
+          >
+            <LoadingButton loading />
+          </Container>
+        )}
       </BaseSelect>
     </FormControl>
   );
